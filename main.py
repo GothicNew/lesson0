4283a9f1264c7a427e2cf01ecdaa0390ab2dc926
Gothic <kalcifer_88@mail.ru>
2024-05-24 14:22:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Задача "Длина слова"
+a = "Длина слова"
+print(len(a))
+
+#Задача "Суммы и разности"
+first = 6
+second = 4
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+#Задача "Среднее арифметическое"
+third = 11
+mean = (first + second + third)//3
+print(mean)
+
+#Задача "Простые строчки"
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + (','), first_string)
+
+#Задача "Сложная формула"
+a = 3
+b = 4
+c = 5
+f = (a * b) + (a * c)
+print((f**3)/2)
